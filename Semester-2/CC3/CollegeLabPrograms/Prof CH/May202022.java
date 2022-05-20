d959a54b3c128769e3595d87de925f8c5e5297a5
Darpan Bhattacharya <54431407+winxtron@users.noreply.github.com>
2022-05-20 16:21:35 +0530

    Update May202022.java

--- Semester-2/CC3/CollegeLabPrograms/Prof CH/May202022.java
+++ Semester-2/CC3/CollegeLabPrograms/Prof CH/May202022.java
@@ -1,8 +1,14 @@
+/**
+ * @author : Darpan Bhattacharya
+ * @details : RKMVCC Rahara, 2nd Semester, B.Sc. CS (hons)
+ * @date : 20.05.2022
+ */
+
 import java.io.*;
 
 public class May202022 {
   public static void main(String args[]) throws Exception {
-    FileHandling.main(args);
+    MyFileHandling.main(args);
   }
 }
 
@@ -44,44 +50,34 @@ class FileHandling {
   }
 }
 
+/* Write a program to copy content from  one file to another, in encrypted form.
+Then decrypt it to a separate file in decrypted form */
+class MyFileHandling {
+  public static void main(String[] args) throws Exception {
+    FileReader fr = new FileReader("/home/first/Documents/715DBCC3Java/FolderMay202022/File1.txt");
+    FileWriter fw = new FileWriter("/home/first/Documents/715DBCC3Java/FolderMay202022/File2.txt");
+    while (true) {
+      int curChar = fr.read();
+      if (curChar != -1) {
+        fw.write((char)(curChar + 69));
+      } else {
+        break;
+      }
+    }
+    fr.close();
+    fw.close();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    fr = new FileReader("/home/first/Documents/715DBCC3Java/FolderMay202022/File2.txt");
+    fw = new FileWriter("/home/first/Documents/715DBCC3Java/FolderMay202022/File3.txt");
+    while (true) {
+      int curChar = fr.read();
+      if (curChar != -1) {
+        fw.write((char)(curChar - 69));
+      } else {
+        break;
+      }
+    }
+    fr.close();
+    fw.close();
+  }
+}
