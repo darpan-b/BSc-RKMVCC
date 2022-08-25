71847ce8ab74431d0591a13cade2665d1302d5f7
Darpan <54431407+winterflame17@users.noreply.github.com>
2022-08-25 15:55:24 +0530

    Add files via upload

---
+++ Semester-3/CC6/Shell programming practical/SD-2022-08-25/palindrome.sh
@@ -0,0 +1,23 @@
+# program to check whether a given number is palindrome
+
+echo "Enter a number"
+read n
+
+m=$n
+rev=0
+
+while [ $n -gt 0 ]
+do
+    d=`expr $n % 10`
+    rev=`expr $rev \* 10 + $d`
+    n=`expr $n / 10`
+done
+
+echo $rev
+
+if [ $rev -eq $m ]
+then
+    echo "Palindrome"
+else
+    echo "Not palindrome"
+fi
