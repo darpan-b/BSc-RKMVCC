77065fd82e3a365ae8fac289b7ef39d38cb74639
Darpan <54431407+winterflame17@users.noreply.github.com>
2022-08-25 15:28:01 +0530

    Add files via upload

---
+++ Semester-3/CC6/Shell programming practical/SD-2022-08-25/add_digits.sh
@@ -0,0 +1,15 @@
+# program to add the digits of a number
+
+echo "Enter a number"
+read n
+
+sum=0
+
+while [ $n -gt 0 ]
+do
+	d=`expr $n % 10`
+	sum=`expr $sum + $d`
+	n=`expr $n / 10`
+done
+
+echo "Sum of digits" $sum
