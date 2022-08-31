eb6c7d1338c9cc671a4d3eee5fdbcab70480225b
Darpan <54431407+winterflame17@users.noreply.github.com>
2022-08-31 16:07:26 +0530

    Add files via upload

---
+++ Semester-3/CC5/Practical Class Programs/2022-08-31/queue_using_array.h
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100001
+int queue[MAX];
+int front = -1, rear = -1;
+
+void push(int data)
+{
+    if (rear == MAX - 1)
+    {
+        printf("Queue is full!\n");
+        return;
+    }
+    queue[++rear] = data;
+    front = (front == -1 ? rear : front);
+}
+
+void pop()
+{
+    if (front == -1)
+    {
+        printf("Queue in empty!\n");
+        return;
+    }
+    front++;
+    front = (front > rear ? -1 : front);
+}
+
+int top()
+{
+    if (front == -1)
+    {
+        printf("Queue in empty!\n");
+        return -1;
+    }
+    return queue[front];
+}
+
+void print()
+{
+    if (front == -1)
+    {
+        printf("Queue in empty!\n");
+        return;
+    }
+    printf("Queue is : ");
+    for (int i = front; i <= rear; i++) printf("%d ", queue[i]);
+    printf("\n");
+}
+
+int empty()
+{
+    return front == -1;
+}
+
