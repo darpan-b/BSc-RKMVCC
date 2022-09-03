16b3ddfbfd4446629eab3dcd1e8bad0a97cb239a
Darpan <54431407+winterflame17@users.noreply.github.com>
2022-09-04 01:49:52 +0530

    Add files via upload

--- Semester-3/CC5/Data Structures Implementation (in C)/circular linked list/circular_linked_list.h
+++ Semester-3/CC5/Data Structures Implementation (in C)/circular linked list/circular_linked_list.h
@@ -1,74 +1,74 @@
+// NOT WORKING (WHY?)
+
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct node
 {
-    int data;
-    struct node *next;
-} node;
+	int data;
+	struct node *next;
+}node;
+
 
 node * create_node(int data)
 {
-    node *newnode = (node *)malloc(sizeof(node));
-    newnode->data = data;
-    newnode->next = NULL;
-    return newnode;
+	node *newnode=(node *)malloc(sizeof(node));
+	newnode->data=data;
+	newnode->next=NULL;
+	return newnode;
 }
 
 void print(node *head)
 {
-    printf("List is :");
-    if (head == NULL) return;
-    node *tmp = head;
-    for (; tmp->next != head; tmp = tmp->next) printf("%d ", tmp->data);
-    printf("%d\n", tmp->data);
+	if(head==NULL) return;
+	printf("List is: ");
+	node *tmp=head;
+	for(; tmp->next!=head; tmp=tmp->next) printf("%d ",tmp->data);
+	printf("%d\n",tmp->data);
 }
 
-void insert_node(node **head, int data, int position)
+void insert_node(node **head,int data,int position)
 {
-    node *newnode = create_node(data);
-    if (*head == NULL)
-    {
-        printf("meow!\n");
-        // KI PROBLEM HOCCHE?
-        newnode->next = newnode;
-        *head = newnode;
-        printf("head data = %d\n", (*head)->data);
-        return;
-    }
-    if (position == 0)
-    {
-        node *tmp = *head;
-        while (tmp->next != *head) tmp = tmp->next;
-        tmp->next = newnode;
-        newnode->next = *head;
-        *head = newnode;
-        return;
-    }
-    node *tmp = *head;
-    int i = 0;
-    for (i = 0; tmp->next != *head && i < position - 1; i++, tmp = tmp->next);
-    newnode->next = tmp->next;
-    tmp->next = newnode;
+	node *newnode=create_node(data);
+	if(*head==NULL)
+	{
+		newnode->next=newnode;
+		*head=newnode;
+		return;
+	}
+	if(position==0)
+	{
+		node *lastnode=*head;
+		while(lastnode->next!=*head) lastnode=lastnode->next;
+		lastnode->next=newnode;
+		newnode->next=*head;
+		*head=newnode;
+		return;
+	}
+	int i=0;
+	node *tmp=*head;
+	for(; i<position-1&&tmp->next!=*head; i++,tmp=tmp->next);
+	newnode->next=tmp->next;
+	tmp->next=newnode;
 }
 
-void delete_node(node **head, int position)
+void delete_node(node **head,int position)
 {
-    if (*head == NULL) return;
-    if (position == 0)
-    {
-        node *tmp = *head;
-        while (tmp->next != *head) tmp = tmp->next;
-        tmp->next = (*head)->next;
-        node *todel = *head;
-        *head = (*head)->next;
-        free(todel);
-        return;
-    }
-    node *tmp = *head;
-    int i = 0;
-    for (i = 0; tmp->next != *head && i < position - 1; i++, tmp = tmp->next);
-    node *todel = tmp->next;
-    tmp->next = tmp->next->next;
-    free(todel);
+	if(*head==NULL) return;
+	if(position==0)
+	{
+		node *lastnode=*head;
+		while(lastnode->next!=*head) lastnode=lastnode->next;
+		lastnode->next=(*head)->next;
+		node *tmp=*head;
+		*head=(*head)->next;
+		free(tmp);
+		return;
+	}
+	int i=0;
+	node *tmp=*head;
+	for(; i<position-1&&tmp->next!=*head; i++,tmp=tmp->next);
+	node *todel=tmp->next;
+	tmp->next=tmp->next->next;
+	free(todel);
 }
