fcd0cc4410251d2d92d9e92ae0f30c61d9cf2d7b
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2022-11-17 15:49:10 +0530

    Create reverse_string.sh

---
+++ Semester-3/CC6/Shell programming practical/SD-2022-11-17/reverse_string.sh
@@ -0,0 +1,8 @@
+# Write a shell script to reverse a string taken from command line argument
+str=$1
+len=`echo $str | wc -c`
+for ((i=len; i>0; i--))
+do
+	ch=`echo $str | cut -c $i`
+	echo -n $ch
+done
