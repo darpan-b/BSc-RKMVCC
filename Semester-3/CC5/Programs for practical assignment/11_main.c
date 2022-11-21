eebbd7404be77ab30e6224e0d8ee0c8f573775c7
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2022-11-21 21:07:41 +0530

    Add files via upload

---
+++ Semester-3/CC5/Programs for practical assignment/11_main.c
@@ -0,0 +1,31 @@
+#include "11_queue.h"
+int main(){
+  while (1){
+    printf("Press 1 to insert element, 2 to pop element from queue.\n");
+    printf("Press 3 to print the queue, 4 to print the topmost element.\n");
+    printf("Press any other integer to exit.\n");
+    printf("Enter your choice:\n");
+    int choice;
+    scanf("%d", &choice);
+    switch (choice){
+    case 1:
+      printf("Enter data to be added to queue:\n");
+      int data;
+      scanf("%d", &data);
+      push(data);
+      break;
+    case 2:
+      pop();
+      break;
+    case 3:
+      print();
+      break;
+    case 4:
+      printf("Topmost element = %d\n", top());
+      break;
+    default:
+      exit(0);
+    }
+  }
+  return 0;
+}
