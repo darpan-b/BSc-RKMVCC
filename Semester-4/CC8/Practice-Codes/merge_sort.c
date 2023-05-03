10c93b53afc5496b51dd693d441970f60d8deae5
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2023-05-03 08:40:38 +0530

    Update merge_sort.c

--- Semester-4/CC8/Practice-Codes/merge_sort.c
+++ Semester-4/CC8/Practice-Codes/merge_sort.c
@@ -2,63 +2,33 @@
 #include <stdlib.h>
 
 void merge(int* arr, int lo, int mid, int hi) {
-    int s1 = mid - lo + 1;
-    int s2 = hi - mid;
-    int* arr1 = (int*) malloc(sizeof(int) * s1);
-    int* arr2 = (int*) malloc(sizeof(int) * s2);
-    for (int i = 0; i < s1; i++) {
-        arr1[i] = arr[i + lo];
-    }
-    for (int i = 0; i < s2; i++) {
-        arr2[i] = arr[i + mid + 1];
-    }
-    int ptr1 = 0, ptr2 = 0;
-    for (int i = lo; i <= hi; i++) {
-        if (ptr1 >= s1) {
-            arr[i] = arr2[ptr2];
-            ptr2++;
-        }
-        else if (ptr2 >= s2) {
-            arr[i] = arr1[ptr1];
-            ptr1++;
-        }
-        else {
-            if (arr1[ptr1] < arr2[ptr2]) {
-                arr[i] = arr1[ptr1];
-                ptr1++;
-            }
-            else {
-                arr[i] = arr2[ptr2];
-                ptr2++;
-            }
-        }
+    int* left_array = (int*)malloc(sizeof(int) * (mid - lo + 1));
+    int* right_array = (int*)malloc(sizeof(int) * (hi - mid));
+    for (int i = lo; i <= mid; i++) left_array[i - lo] = arr[i];
+    for (int i = mid + 1; i <= hi; i++) right_array[i - mid - 1] = arr[i];
+    int lptr = 0, rptr = 0, cidx = lo;
+    while (lptr < (mid - lo + 1) || rptr < (hi - mid)) {
+        if (lptr >= mid - lo + 1) arr[cidx++] = right_array[rptr++];
+        else if (rptr >= hi - mid) arr[cidx++] = left_array[lptr++];
+        else arr[cidx++] = (left_array[lptr] <= right_array[rptr] ? left_array[lptr++] : right_array[rptr++]);
     }
 }
 
 void merge_sort(int* arr, int lo, int hi) {
-    if (lo >= hi) {
-        return;
-    }
+    if (lo >= hi) return;
     int mid = (lo + hi) / 2;
-    merge_sort(arr, lo, mid);
-    merge_sort(arr, mid + 1, hi);
+    merge_sort(arr, lo, mid); merge_sort(arr, mid + 1, hi);
     merge(arr, lo, mid, hi);
 }
 
 int main() {
     printf("Enter the number of elements in the array:\n");
-    int n;
-    scanf("%d", &n);
-    int* arr = (int*) malloc(sizeof(int) * n);
-    printf("Enter %d numbers:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    merge_sort(arr, 0, n - 1);
+    int len; scanf("%d", &len);
+    int* arr = (int*)malloc(sizeof(int) * len);
+    printf("Enter %d elements\n", len);
+    for (int i = 0; i < len; i++) scanf("%d", &arr[i]);
+    merge_sort(arr, 0, len - 1);
     printf("Sorted array = ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    for (int i = 0; i < len; i++) printf("%d ", arr[i]); printf("\n");
     return 0;
 }
