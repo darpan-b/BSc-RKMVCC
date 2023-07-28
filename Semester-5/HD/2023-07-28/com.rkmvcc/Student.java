c26b76545fac2a7b584ac8c146be0fc25a47ca1f
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2023-07-28 11:23:04 +0530

    Create Student.java

---
+++ Semester-5/HD/2023-07-28/com.rkmvcc/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+  int roll, id;
+  String name, course;
+  
+  void registerStudent(int roll, int id, String name, String course) {
+    this.roll = roll;
+    this.id = id;
+    this.name = name;
+    this.course = course;
+  }
+  
+  @Override
+  public String toString() {
+    return "Student [roll = " + roll + " id = " + id + " name = " + name + " course = " + course + "]";
+  }
+  
+  public static void main(String[] args) {
+    Student s = new Student();
+    s.registerStudent(1, 101, "Ankit", "Java Bean");
+  }
+}
