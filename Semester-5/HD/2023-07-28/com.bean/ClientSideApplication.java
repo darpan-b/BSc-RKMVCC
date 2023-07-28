616a1360be8aaf7a5dd89f4f6b414c5c4914dd70
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2023-07-28 12:27:48 +0530

    Add files via upload

---
+++ Semester-5/HD/2023-07-28/com.bean/ClientSideApplication.java
@@ -0,0 +1,12 @@
+package com.bean;
+
+public class ClientSideApplication {
+  public static void main(String[] args) {
+    ServerSideApplication s = new ServerSideApplication();
+    s.setName("Ankit");
+    s.setId(101);
+    s.setCourse("Java Bean");
+    s.setRoll(1);
+  }
+}
+
