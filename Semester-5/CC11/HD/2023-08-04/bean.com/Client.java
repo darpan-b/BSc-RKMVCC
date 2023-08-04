11972c7463b1e7ce400572053be4fdc759de572a
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2023-08-04 12:30:28 +0530

    Update Client.java

--- Semester-5/CC11/HD/2023-08-04/bean.com/Client.java
+++ Semester-5/CC11/HD/2023-08-04/bean.com/Client.java
@@ -1,5 +1,7 @@
 package bean.com;
 
+import java.util.*;
+
 public class Client {
 	public static void main(String[] args) {
 		BankServer bankServer = new BankServer();
@@ -10,6 +12,15 @@ public class Client {
 		bankServer.setPassword("ffsrfseff");
 		Client c = new Client();
 		c.printClient(bankServer);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Amount to credit:");
+		double toCredit = sc.nextDouble();
+		bankServer.credit(toCredit);
+		System.out.println("Current balance = " + bankServer.getBalance());
+		System.out.println("Amount to debit:");
+		double toDebit = sc.nextDouble();
+		bankServer.debit(toDebit);
+		System.out.println("Current balance = " + bankServer.getBalance());
 	}
 	
 	public void printClient(BankServer bServer) {
