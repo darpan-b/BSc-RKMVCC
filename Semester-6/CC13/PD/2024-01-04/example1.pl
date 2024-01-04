b9192c84c81190a09af7e4052c16c15cdad5e533
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2024-01-04 15:58:39 +0530

    Update example1.pl

--- Semester-6/CC13/PD/2024-01-04/example1.pl
+++ Semester-6/CC13/PD/2024-01-04/example1.pl
@@ -1,8 +1,8 @@
-food(burger).
-food(sandwich).
-food(pizza).
-lunch(sandwich).
-dinner(pizza).
+food(burger). % burger is a food
+food(sandwich). % sandwich is a food
+food(pizza). % pizza is a food
+lunch(sandwich). % sandwich is a lunch
+dinner(pizza). % pizza is a dinner
 
-meal(X) :- food(X).
+meal(X) :- food(X). % Every food is a meal OR Anything is a meal if it is a food
 
