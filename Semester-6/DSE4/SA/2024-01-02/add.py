09d2d46211c1c5c6cebae0bede4bd507b121a18e
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2024-01-04 15:36:33 +0530

    Create add.py

---
+++ Semester-6/DSE4/SA/2024-01-02/add.py
@@ -0,0 +1,14 @@
+# First program
+a = 10
+b = 15
+c = a+b
+print("The sum is:", c)
+
+
+# Second program
+x = int(input("Enter first value: "))
+y = int(input("Enter second value: "))
+z = x+y
+print("The sum is:", z)
+
+
